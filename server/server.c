8b6e27f004a2b8a18f9e3319ecfccce07d55d207
AirCraft009 <coconraupe@gmail.com>
2025-10-08 22:07:05 +0200

    finished Csocket working on server

--- server/server.c
+++ server/server.c
@@ -1,4 +1,56 @@
-
+#include "socket.c"
 #include "server.h"
+#include <pthread.h>
+#include <stdio.h>
 
+typedef struct{
+    Csocket sock;
+    int port;
+    bool listening;
+}server;
+void Listen(server * server);
+void Accept(server * server);
+void handleConnection(SOCKET conn);
+
+server * CreateServer(int port) {
+    server * server = malloc(sizeof(server));
+    if (!server) return NULL;
+    server->listening = false;
+    server->port = port;
+    return server;
+}
+
+void Listen(server * server) {
+    server->listening = true;
+    ListenToOne(&server->sock);
+}
+
+void Accept(server *server) {
+    if (!server->listening) {
+        Listen(server);
+    }
+    while (server->listening) {
+        SOCKET connSocket = AcceptConn(&server->sock);
+        if (connSocket == (SOCKET) 0 ) {
+            return;
+        }
+        handleConnection(connSocket);
+
+    }
+}
+
+void handleConnection(SOCKET conn) {
+
+}
+
+void DestroyServer(server * server) {
+    if (!server) return;
+    CloseSocket(&server->sock);
+}
+
+void FreeServer(server * server) {
+    if (!server) return;
+    DestroyServer(server);
+    free(server);
+}
 
--- server/socket.c
+++ server/socket.c
@@ -4,15 +4,40 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <stdio.h>
+#include "server.h"
 
-struct Csocket {
+typedef struct{
     WSADATA wsaData;
     SOCKET serverSock, clientSock;
     struct sockaddr_in addr, clientAddr;
     int clientAddrSize;
-};
+    int Port;
+} Csocket;
 
-int initSocket(struct Csocket* csocket) {
+void CloseSocket(Csocket * csocket);
+Csocket * CreateSocket(int port);
+int InitSocket(Csocket * csocket);
+int ListenToOne(Csocket * csocket);
+SOCKET AcceptConn(Csocket * csocket);
+void CsocketFree(Csocket * csocket);
+
+
+
+Csocket *CreateSocket(int port) {
+    Csocket *cs = malloc(sizeof(Csocket));
+
+    if (!cs) return NULL;
+    cs->serverSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (cs->serverSock == INVALID_SOCKET) {
+        free(cs);
+        return NULL;
+    }
+    // I don't know if this is the correct choice to only set port and then have everything else be done in InitSocket!!
+    cs-> Port = port;
+    return cs;
+}
+
+int InitSocket(Csocket* csocket) {
     csocket->clientAddrSize = sizeof(csocket->clientAddr);
 
     // initialize winsock (WSAStartup inject DLL
@@ -22,7 +47,7 @@ int initSocket(struct Csocket* csocket) {
         return 1;
     }
 
-    //create the tcp csocket pass AF_INET which means that it's of the ipv4 type
+    //create the tcp csocket pass AF_INET which means that it'cs of the ipv4 type
     csocket->serverSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (csocket->serverSock == INVALID_SOCKET) {
         printf("WSAStartup failed: %d\n", WSAGetLastError());
@@ -32,59 +57,55 @@ int initSocket(struct Csocket* csocket) {
 
     // initialize the memory at the addr to 0 before setting fields (because remember when mem is freed it doesn't change what's written only removes the reference)
     memset(&csocket->addr, 0, sizeof(csocket->addr));
+    //specify ipv4
+    csocket->addr.sin_family = AF_INET;
+    // allow it to connect to any interface
+    csocket->addr.sin_addr.s_addr = INADDR_ANY;
+    csocket->addr.sin_port = htons(csocket->Port);
 
-
-
+    //GERMAN so I can remember better
+    // wir casten die struct sockaddr_in zu einem (sockaddr*), weil die bind method den generic type verwendet
+    // wir übergeben danach die länge damit trotzdem alle daten gelesen werden können
+    if (bind(csocket->serverSock, (struct sockaddr*)&csocket->addr, sizeof(csocket->addr)) == SOCKET_ERROR) {
+        printf("WSAStartup failed: %d\n", WSAGetLastError());
+        closesocket(csocket->serverSock);
+        WSACleanup();
+        return 1;
+    }
+    return 0;
 }
 
-int main(void) {
-
-
-    // Prepare address
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY; // all interfaces
-    addr.sin_port = htons(PORT);// port 8080
-
-    // Bind socket
-    if (bind(serverSock, (struct sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR) {
-        printf("bind failed: %d\n", WSAGetLastError());
-        closesocket(serverSock);
-        WSACleanup();
-        return 1;
-    }
-
-    // Listen for connections
-    if (listen(serverSock, 5) == SOCKET_ERROR) {
+int ListenToOne(Csocket* csocket) {
+    // start listening
+    // backlog is the ammount of connection the kernel should queue before connecting
+    if (listen(csocket->serverSock, 5) == SOCKET_ERROR) {
         printf("listen failed: %d\n", WSAGetLastError());
-        closesocket(serverSock);
+        closesocket(csocket->serverSock);
         WSACleanup();
         return 1;
     }
+    return 0;
+}
 
-    printf("Waiting for connection on port 8080...\n");
+void CsocketFree(Csocket* csocket) {
+    if (!csocket) return;
+    CloseSocket(csocket);
+}
 
-    // Accept a connection (blocks until a client connects)
-    clientSock = accept(serverSock, (struct sockaddr*)&clientAddr, &clientAddrSize);
+SOCKET AcceptConn(Csocket* csocket) {
+    // Accept incoming connection
+    SOCKET clientSock = accept(csocket->serverSock, (struct sockaddr*)&csocket->clientAddr, &csocket->clientAddrSize);
     if (clientSock == INVALID_SOCKET) {
         printf("accept failed: %d\n", WSAGetLastError());
-        closesocket(serverSock);
+        closesocket(csocket->clientSock);
         WSACleanup();
-        return 1;
+        return 0;
     }
+    return clientSock;
+}
 
-    // Print client info
-    char clientIP[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &clientAddr.sin_addr, clientIP, sizeof(clientIP));
-    printf("Client connected from %s:%d\n", clientIP, ntohs(clientAddr.sin_port));
-
-    // Send a message
-    const char *msg = "Hello from server!\n";
-    send(clientSock, msg, (int)strlen(msg), 0);
-
-    // Close sockets
-    closesocket(clientSock);
-    closesocket(serverSock);
+void CloseSocket(Csocket* csocket) {
+    closesocket(csocket->serverSock);
+    closesocket(csocket->clientSock);
     WSACleanup();
-    return 0;
 }
