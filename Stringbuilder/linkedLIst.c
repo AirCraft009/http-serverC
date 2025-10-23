a2353d61dbea8de0bc648519df457948ac1e8a85
AirCraft009 <coconraupe@gmail.com>
2025-10-23 21:40:40 +0200

    linked list added getNode

--- Stringbuilder/linkedLIst.c
+++ Stringbuilder/linkedLIst.c
@@ -4,17 +4,96 @@
 
 #include "linkedLIst.h"
 
+#include <assert.h>
+#include <stdlib.h>
+
 typedef struct Node{
     struct Node *next;
     char *data;
+    int index;
 }Node;
 
 typedef struct LinkedList {
     Node *head;
     Node *tail;
-    Node size;
+    int size;
     Node *current;
 }LinkedList;
+
+
+Node * getNodeCurrentAssured(LinkedList * list,  int index);
+
+LinkedList * createLinkedList() {
+    LinkedList *list = malloc(sizeof(LinkedList));
+    list->head = nullptr;
+    list->tail = nullptr;
+    list->current = nullptr;
+    return list;
+}
+
+Node * createNode(char * data) {
+    Node *node = malloc(sizeof(Node));
+    node->data = data;
+    return node;
+}
+
+void addNode(Node * node, LinkedList * list) {
+    assert(node);
+    if (list->head != nullptr) {
+        list->head->next = node;
+    }
+    list->head = node;
+    node->index = list->size;
+    list->size ++;
+}
+
+void addNewNode(char * data, LinkedList * list) {
+    Node * newNode = createNode(data);
+    addNode(newNode, list);
+}
+
+Node * nextNode(LinkedList * list) {
+    // returns the topmost note if called while current == list.head
+    assert(list->current);
+    if (list->current == list->head) {
+        return list->head;
+    }
+    Node * nextNode = list->current->next;
+    list->current = nextNode;
+    return nextNode;
+}
+
+Node * getNode(LinkedList * list, int index) {
+    if (index >= list->current->index) {
+        return getNodeCurrentAssured(list, index);
+    }
+    if (list->size <= index) {
+        return nullptr;
+    }
+    Node * iterNode = list->tail;
+    for (int i = 0; i < index; i++) {
+        iterNode = iterNode->next;
+    }
+    return iterNode;
+}
+//this method does the same thing as getNode.
+//the only difference is that getNode doesn't assure that list.current
+//is actually the node that is currently in use
+Node * getNodeCurrentAssured(LinkedList * list,  int index) {
+    //index out of bounds
+    if (list->size <= index) {
+        return nullptr;
+    }
+    Node * iterNode = list->current;
+    for (int i = iterNode->index; i < index; i++) {
+        iterNode = iterNode->next;
+    }
+    return iterNode;
+}
+
+
+
+
 
 
 
